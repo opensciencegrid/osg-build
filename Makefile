571799a0678cb2d81ec31ba76bc2e1d3a05ae815
Matyas Selmeci <matyas@cs.wisc.edu>
2011-07-22 16:31:10 +0000

    Merged vdt-build changed from vdt-build-matyas branch to trunk svn merge -r 12348:12367 file:///p/vdt/workspace/svn/software/vdt-build/branches/vdt-build-matyas/ .
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/vdt-build/trunk@12368 4e558342-562e-0410-864c-e07659590f8c

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 0.0.9
+VERSION = 0.0.10
 PYFILES = $(wildcard *.py)
 MAIN_SCRIPT = vdt-build
 PUSH_SCRIPT = push-rpm-to-vdt
--- platform-post.py
+++ platform-post.py
@@ -6,14 +6,20 @@ import os
 import subprocess
 import sys
 
-class PlatformPostError(Exception):
+class CalledProcessError(Exception):
     pass
 
 def checked_call(*args):
+    if type(args[0]) == type(''):
+        cmd = args[0]
+    elif type(args[0]) == type([]) or type(args[0]) == type(()):
+        cmd = "'" + "' '".join(args[0]) + "'"
+    print "Running " + cmd
     err = subprocess.call(*args)
+    print "Subprocess returned " + str(err)
     if err:
-        raise PlatformPostError("subprocess.call(" + str(args) +
-                                ") returned " + str(err) + "!")
+        raise CalledProcessError("subprocess.call(" + str(args) + ") returned " +
+                                 str(err) + "!")
 
 
 # Turn off buffering for stdout/stderr
@@ -31,8 +37,15 @@ checked_call(["tar", "xvzf", "results.tar.gz"])
 
 files = glob("results/*")
 
-for f in files:
-    checked_call(["scp", f, options.host + ":" + options.results_dir])
+try:
+    for f in files:
+        checked_call(["scp", f, options.host + ":" + options.results_dir])
+except CalledProcessError, e:
+    print >>sys.stderr, "Unable to scp files to " + options.host + ":" + options.results_dir
+    print >>sys.stderr, "Do you have your ssh keys set up?"
+    print >>sys.stderr, "See https://twiki.grid.iu.edu/bin/view/Documentation/VDTRPMBatlabBuild"
+    
+    sys.exit(1)
 
 if options.yum_base:
     print("Pushing to yum repos under", options.yum_base)

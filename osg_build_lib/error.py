36ebc06ecd9444bdeed6d314ca6cf3a2a3be76f8
Matyas Selmeci <matyas@cs.wisc.edu>
2011-12-08 03:08:28 +0000

    merged change 13685
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-build/trunk@13686 4e558342-562e-0410-864c-e07659590f8c

---
+++ osg_build_lib/error.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import os
+import traceback
+
+class Error(Exception):
+    """Base class for expected exceptions"""
+    def __init__(self, msg, tb=None):
+        self.msg = msg
+        if tb is None:
+            self.traceback = traceback.format_exc()
+
+    def __repr__(self):
+        return repr((self.msg, self.traceback))
+
+    def __str__(self):
+        return str(self.msg)
+
+
+class GlobNotFoundError(Error):
+    def __init__(self, globtext):
+        msg = "Couldn't find file matching '%s'." % globtext
+        Error.__init__(self, msg)
+
+
+class FileNotFoundError(Error):
+    def __init__(self, fname, searchpath=None):
+        msg = "Couldn't find file named '%s'." % fname
+        if searchpath:
+            Error.__init__(self, msg + " Search path was:\n%s" % os.pathsep.join(searchpath))
+        else:
+            Error.__init__(self, msg)
+
+
+class ProgramNotFoundError(Error):
+    def __init__(self, program):
+        msg = "Couldn't find required program '%s'." % program
+        if program.find("/") == -1:
+            Error.__init__(self, msg + " $PATH was:\n%s" % os.environ['PATH'])
+        else:
+            Error.__init__(self, msg)
+
+
+class OSGBuildError(Error):
+    def __init__(self, msg, tb=None):
+        Error.__init__(self, "Error in build step: " + msg, tb)
+
+
+class OSGPrebuildError(Error):
+    def __init__(self, msg, tb=None):
+        Error.__init__(self, "Error in pre-build step: " + msg, tb)
+
+
+class UsageError(Error):
+    def __init__(self, msg, tb=None):
+        Error.__init__(self, "Usage error: " + msg + "\n", tb)
+
+
+class KojiError(Error):
+    def __init__(self, msg, tb=None):
+        Error.__init__(self, "Koji error: " + msg, tb)
+
+
+class MockError(Error):
+    def __init__(self, msg, tb=None):
+        Error.__init__(self, "Mock error: " + msg, tb)
+
+

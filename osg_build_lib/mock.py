d60c2ee5f2de6bb8096a4448ad896d3c377184d4
matyas <matyas@4e558342-562e-0410-864c-e07659590f8c>
2011-12-08 03:05:41 +0000

    osg-build-0.3.0 commit: merged changes from branches/matyas svn merge file:///p/vdt/workspace/svn/software/osg-build/branches/matyas -r13641:13683
    git-svn-id: file:///p/vdt/workspace/svn/software/osg-build/trunk@13684 4e558342-562e-0410-864c-e07659590f8c

--- osg_build_lib/mock.py
+++ osg_build_lib/mock.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+from fnmatch import fnmatch
+from glob import glob
+import grp
 import os
 import re
 import shutil
@@ -5,67 +9,133 @@ import string
 import subprocess
 import sys
 
-from osg_build_lib.utils import *
+from osg_build_lib.error import MockError
+from osg_build_lib.utils import checked_call, sbacktick, unchecked_call, unslurp
 
 
 def get_mock_version():
     """Return mock version as a 2-element tuple (major, minor)"""
-    query = subprocess.Popen(
-        ["rpm", "-q", "mock"], stdout=subprocess.PIPE).communicate()[0]
+    query, ret = sbacktick("rpm -q mock")
     match = re.match(r'''mock-((?:\d+\.?)+)-''', query)
-    if not match:
+    if ret != 0 or not match:
         print >>sys.stderr,"Unable to determine the mock version"
-        print >>sys.stderr,"Make sure mock in installed (yum install mock)"
-        raise Exception(query) # TODO do better than this
+        print >>sys.stderr,"Make sure mock is installed (yum install mock)"
+        raise MockError(query)
     version = [int(x) for x in match.group(1).split(".")]
     return tuple(version)
 
 
-def make_mock_config(arch, cfg_dir, mockver=None, dist='osg', index=''):
-    """Autogenerate a mock config for arch 'arch'. 'mockver' is needed because
-    mock config file format changed incompatibly somewhere in mock 0.8 or so
-    and we need support for both 0.6.x (centos) and 1.x (batlab, epel).
-
-    """
+def make_mock_config(arch, cfg_path, dist):
+    """Autogenerate a mock config for arch 'arch'."""
     if re.match(r'i[3-6]86', arch):
         basearch = 'i386'
     else:
         basearch = arch
-    if index != '':
-        index = '.' + index
-    cfg_name = "mock-auto-%s%s" % (arch, index)
-    cfg_path = os.path.join(cfg_dir, cfg_name + ".cfg")
-    if mockver is None:
-        mockver = get_mock_version()
+    
+    cfg_abspath = os.path.abspath(cfg_path)
+    cfg_name = re.sub(r'\.cfg$', '', os.path.basename(cfg_abspath))
+    #cfg_dir = os.path.dirname(cfg_abspath)
 
+    mockver = get_mock_version()
     if mockver < (0, 8):
         template = OLD_MOCK_CFG_TEMPLATE
     else:
         template = NEW_MOCK_CFG_TEMPLATE
 
-
-    unslurp(cfg_path,
+    unslurp(cfg_abspath,
             template.safe_substitute(
-                NAME=cfg_name, ARCH=arch, BASEARCH=basearch,
+                NAME=cfg_name,
+                ARCH=arch,
+                BASEARCH=basearch,
                 DIST=dist))
+    
+    #if mockver >= (0, 8):
+    #    link_mock_extra_config_files(cfg_dir)
 
-    return cfg_name
+    return cfg_abspath
 
 
-def copy_mock_extra_config_files(cfg_dir, mockver=None, force=False):
-    if mockver is None:
-        mockver = get_mock_version()
+def make_mock_config_from_koji(koji_obj, arch, cfg_path, tag, dist):
+    mockver = get_mock_version()
+    if mockver < (0, 8):
+        raise MockError("Mock version too old to use a mock config from a koji buildroot. Needs to be at least 0.8")
 
-    if mockver >= (0, 8):
-        for filename in ['site-defaults.cfg', 'logging.ini']:
-            system_filepath = os.path.join("/etc/mock", filename)
-            local_filepath = os.path.join(cfg_dir, filename)
-            if os.path.exists(system_filepath) and \
-                    (force or not os.path.exists(local_filepath)):
-                shutil.copy(system_filepath, local_filepath)
-    else:
-        # Not necessary
-        pass
+    cfg_abspath = os.path.abspath(cfg_path)
+    cfg_name = re.sub(r'\.cfg$', '', os.path.basename(cfg_abspath))
+    #cfg_dir = os.path.dirname(cfg_abspath)
+
+    koji_obj.mock_config(arch, tag, dist, cfg_abspath, cfg_name)
+
+    #link_mock_extra_config_files(cfg_dir)
+
+    return cfg_abspath
+
+
+def link_mock_extra_config_files(cfg_dir):
+    for filename in ['site-defaults.cfg', 'logging.ini']:
+        system_filepath = os.path.join("/etc/mock", filename)
+        local_filepath = os.path.join(cfg_dir, filename)
+        if os.path.exists(system_filepath) and not os.path.exists(local_filepath):
+            os.symlink(system_filepath, local_filepath)
+
+
+class Mock(object):
+
+    def __init__(self, cfg_path=None, target_arch=None):
+        self.mock_cmd = ['mock']
+        if cfg_path:
+            cfg_abspath = os.path.abspath(cfg_path)
+            cfg_abspath_no_ext = re.sub(r'\.cfg$', '', cfg_abspath)
+            #self.cfg_dir = os.path.dirname(cfg_abspath_no_ext)
+            self.cfg_name = os.path.basename(cfg_abspath_no_ext)
+
+            if not os.path.isfile(cfg_abspath):
+                raise MockError("Couldn't find mock config file at " + cfg_abspath)
+
+            #self.mock_cmd += ['--configdir', self.cfg_dir, '-r', self.cfg_name]
+            # The cfg file passed to mock is always relative to /etc/mock
+            self.mock_cmd += ['-r', "../../"+cfg_abspath_no_ext]
+        else:
+            #self.cfg_dir = None
+            self.cfg_name = None
+
+        self.target_arch = target_arch
+
+        try:
+            mock_gid = grp.getgrnam('mock').gr_gid
+        except KeyError:
+            raise MockError("The mock group does not exist on this system!")
+        if mock_gid not in os.getgroups():
+            raise MockError(
+"""You are not able to do a mock build on this machine because you are not in the mock group.
+/etc/group must be edited and your username must be added to the mock group.
+You might need to log out and log in for the changes to take effect""")
+
+
+    def rebuild(self, resultdir, srpm, extra_opts=None):
+        rebuild_cmd = self.mock_cmd + ['--resultdir',
+                                       resultdir,
+                                       '--no-cleanup-after',
+                                       'rebuild',
+                                       srpm]
+        if self.target_arch:
+            rebuild_cmd += ['--arch', self.target_arch]
+
+        ret = unchecked_call(rebuild_cmd)
+        if ret:
+            raise MockError('Mock build failed (command was: ' +
+                                ' '.join(rebuild_cmd) + ')')
+        
+        # TODO: Parse the mock logs/output instead of using glob.
+        rpms = [x for x in glob(os.path.join(resultdir, "*.rpm"))
+                if not fnmatch(x, "*.src.rpm")]
+
+        return rpms
+
+
+    def clean(self):
+        checked_call(self.mock_cmd + ["clean"])
+
         
 #
 #

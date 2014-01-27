9bba2d5e34d03e6fd036a54f6beeb1cfbd4e3ce2
Matyas Selmeci <matyas@cs.wisc.edu>
2014-01-27 19:49:05 +0000

    Add multipromote tests; some test tweaks (SOFTWARE-1289)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-build/trunk@18661 4e558342-562e-0410-864c-e07659590f8c

--- osgbuild/test/test_osgpromote.py
+++ osgbuild/test/test_osgpromote.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python2
 
+import logging
 import unittest
 
 from osgbuild import promoter
 
+log = logging.getLogger('osgpromote')
+log.setLevel(logging.ERROR)
 
 TAGS = ['el6-gt52',
         'hcc-el5',
@@ -50,16 +53,16 @@ TAGS = ['el6-gt52',
 class TestUtil(unittest.TestCase):
     buildnvr = "osg-build-1.3.2-1.osg32.el5"
     def test_split_nvr(self):
-        self.assertEqual(promoter.split_nvr(self.buildnvr), ('osg-build', '1.3.2', '1.osg32.el5'))
+        self.assertEqual(('osg-build', '1.3.2', '1.osg32.el5'), promoter.split_nvr(self.buildnvr))
 
     def test_split_dver(self):
-        self.assertEqual(promoter.split_dver(self.buildnvr), ('osg-build-1.3.2-1.osg32', 'el5'))
+        self.assertEqual(('osg-build-1.3.2-1.osg32', 'el5'), promoter.split_dver(self.buildnvr))
 
     def test_split_repo_dver(self):
-        self.assertEqual(promoter.split_repo_dver(self.buildnvr), ('osg-build-1.3.2-1', 'osg32', 'el5'))
-        self.assertEqual(promoter.split_repo_dver('foo-1-1.osg'), ('foo-1-1', 'osg', ''))
-        self.assertEqual(promoter.split_repo_dver('foo-1-1.el5'), ('foo-1-1', '', 'el5'))
-        self.assertEqual(promoter.split_repo_dver('foo-1-1'), ('foo-1-1', '', ''))
+        self.assertEqual(('osg-build-1.3.2-1', 'osg32', 'el5'), promoter.split_repo_dver(self.buildnvr))
+        self.assertEqual(('foo-1-1', 'osg', ''), promoter.split_repo_dver('foo-1-1.osg'))
+        self.assertEqual(('foo-1-1', '', 'el5'), promoter.split_repo_dver('foo-1-1.el5'))
+        self.assertEqual(('foo-1-1', '', ''), promoter.split_repo_dver('foo-1-1'))
 
 
 class TestRouteDiscovery(unittest.TestCase):
@@ -68,30 +71,30 @@ class TestRouteDiscovery(unittest.TestCase):
         self.routes = self.route_discovery.get_routes()
 
     def test_static_route(self):
-        self.assertEqual(self.routes['hcc'][0], 'hcc-%s-testing')
-        self.assertEqual(self.routes['hcc'][1], 'hcc-%s-release')
+        self.assertEqual('hcc-%s-testing', self.routes['hcc'][0])
+        self.assertEqual('hcc-%s-release', self.routes['hcc'][1])
 
         f, t = self.routes['hcc'][0:2]
-        self.assertEqual(f, 'hcc-%s-testing')
-        self.assertEqual(t, 'hcc-%s-release')
+        self.assertEqual('hcc-%s-testing', f)
+        self.assertEqual('hcc-%s-release', t)
 
     def test_detected_route(self):
-        self.assertEqual(self.routes['3.2-testing'][0], 'osg-3.2-%s-development')
-        self.assertEqual(self.routes['3.2-testing'][1], 'osg-3.2-%s-testing')
+        self.assertEqual('osg-3.2-%s-development', self.routes['3.2-testing'][0])
+        self.assertEqual('osg-3.2-%s-testing', self.routes['3.2-testing'][1])
 
     def test_route_alias(self):
         for idx in [0, 1]:
             self.assertEqual(self.routes['testing'][idx], self.routes['3.2-testing'][idx])
 
     def test_new_static_route(self):
-        self.assertEqual(self.routes['hcc'].from_tag_hint, 'hcc-%s-testing')
-        self.assertEqual(self.routes['hcc'].to_tag_hint, 'hcc-%s-release')
-        self.assertEqual(self.routes['hcc'].repo, 'hcc')
+        self.assertEqual('hcc-%s-testing', self.routes['hcc'].from_tag_hint)
+        self.assertEqual('hcc-%s-release', self.routes['hcc'].to_tag_hint)
+        self.assertEqual('hcc', self.routes['hcc'].repo)
 
     def test_new_detected_route(self):
-        self.assertEqual(self.routes['3.2-testing'].from_tag_hint, 'osg-3.2-%s-development')
-        self.assertEqual(self.routes['3.2-testing'].to_tag_hint, 'osg-3.2-%s-testing')
-        self.assertEqual(self.routes['3.2-testing'].repo, 'osg32')
+        self.assertEqual('osg-3.2-%s-development', self.routes['3.2-testing'].from_tag_hint)
+        self.assertEqual('osg-3.2-%s-testing', self.routes['3.2-testing'].to_tag_hint)
+        self.assertEqual('osg32', self.routes['3.2-testing'].repo)
 
     def test_new_route_alias(self):
         for key in 'from_tag_hint', 'to_tag_hint', 'repo':
@@ -104,24 +107,32 @@ class TestRouteDiscovery(unittest.TestCase):
 
 class MockKojiHelper(promoter.KojiHelper):
     tagged_builds_by_tag = {
+            'osg-3.1-el5-development': [
+                {'nvr': 'foobar-2000-1.osg31.el5', 'latest': True},
+                ],
+            'osg-3.1-el6-development': [
+                {'nvr': 'foobar-2000-1.osg31.el6', 'latest': True},
+                {'nvr': 'otherrejectme-1-1.osg31.el6', 'latest': True},
+                ],
             'osg-3.2-el5-development': [
                 {'nvr': 'foobar-1999-1.osg32.el5', 'latest': False},
                 {'nvr': 'foobar-2000-1.osg32.el5', 'latest': True},
-                {'nvr': 'reject-1-1.osg32.el5', 'latest': True},
+                {'nvr': 'rejectme-1-1.osg32.el5', 'latest': True},
                 ],
             'osg-3.2-el6-development': [
                 {'nvr': 'foobar-1999-1.osg32.el6', 'latest': False},
                 {'nvr': 'foobar-2000-1.osg32.el6', 'latest': True},
-                {'nvr': 'reject-2-1.osg32.el6', 'latest': True},
+                {'nvr': 'rejectme-2-1.osg32.el6', 'latest': True},
+                {'nvr': 'otherrejectme-2-1.osg32.el6', 'latest': True},
                 ],
             }
     tagged_packages_by_tag = {
             'osg-3.2-el5-development': [
                 'foobar',
-                'reject'],
+                'rejectme'],
             'osg-3.2-el6-development': [
                 'foobar',
-                'reject'],
+                'rejectme'],
             }
 
     def get_tagged_packages(self, tag):
@@ -160,28 +171,39 @@ class TestPromoter(unittest.TestCase):
         route = self.routes['testing']
         prom = self._makePromoter(route)
         prom.add_promotion('foobar')
-        prom.add_promotion('reject')
-        self.assertFalse('reject-1-1.osg32.el5' in prom.tag_pkg_args[route.to_tag_hint % 'el5'])
+        prom.add_promotion('rejectme')
+        self.assertFalse('rejectme-1-1.osg32.el5' in prom.tag_pkg_args[route.to_tag_hint % 'el5'])
 
     def test_reject_add_with_ignore(self):
         route = self.routes['testing']
         prom = self._makePromoter(route)
         prom.add_promotion('foobar')
-        prom.add_promotion('reject', ignore_rejects=True)
-        self.assertTrue('reject-1-1.osg32.el5' in prom.tag_pkg_args[route.to_tag_hint % 'el5'])
-        self.assertTrue('reject-2-1.osg32.el6' in prom.tag_pkg_args[route.to_tag_hint % 'el6'])
+        prom.add_promotion('rejectme', ignore_rejects=True)
+        self.assertTrue('rejectme-1-1.osg32.el5' in prom.tag_pkg_args[route.to_tag_hint % 'el5'])
+        self.assertTrue('rejectme-2-1.osg32.el6' in prom.tag_pkg_args[route.to_tag_hint % 'el6'])
 
     def test_new_reject(self):
         route = self.routes['testing']
         prom = self._makePromoter(route)
-        prom.add_promotion('reject')
+        prom.add_promotion('rejectme')
         rejs = prom.get_rejects()
-        self.assertEqual(len(rejs), 1)
-        self.assertTrue(rejs[0].pkg_or_build == 'reject')
-        self.assertTrue(rejs[0].reason == promoter.Rejects.REASON_DISTINCT_ACROSS_DVERS)
+        self.assertEqual(1, len(rejs))
+        self.assertEqual('rejectme', rejs[0].pkg_or_build)
+        self.assertEqual(promoter.Reject.REASON_DISTINCT_ACROSS_DVERS, rejs[0].reason)
 
+    def test_multi_promote(self):
+        route1 = self.routes['3.1-testing']
+        route2 = self.routes['3.2-testing']
+        prom = self._makePromoter([route1, route2])
+        prom.add_promotion('foobar')
+        self.assertTrue(('osg-3.1-el5-testing', 'foobar-2000-1.osg31.el5') in prom.tag_pkg_args)
+        self.assertTrue(('osg-3.2-el6-testing', 'foobar-2000-1.osg32.el6') in prom.tag_pkg_args)
 
-
+    def test_cross_dist_reject(self):
+        prom = self._makePromoter([self.routes['3.1-testing'], self.routes['3.2-testing']], ['el6'])
+        prom.add_promotion('otherrejectme')
+        rejs = prom.get_rejects()
+        self.assertEqual(2, len(rejs))
 
 
 if __name__ == '__main__':

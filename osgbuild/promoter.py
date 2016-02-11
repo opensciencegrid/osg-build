b77fe8f8710e3389f898f2cb6976453601328835
Matyas Selmeci <matyas@cs.wisc.edu>
2016-02-11 23:35:05 +0000

    Change the layout of the osg-promote table for JIRA (SOFTWARE-2116)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-build/trunk@22469 4e558342-562e-0410-864c-e07659590f8c

--- osgbuild/promoter.py
+++ osgbuild/promoter.py
@@ -533,18 +533,22 @@ def write_jira(kojihelper, promoted_builds, routes, out=None):
     """Write input suitable for embedding into a JIRA ticket.
     """
     # Format
-    # | TAG | build-1.2.osg31.el5 |
+    # | build-1.2.osg31.el5 | TAG |
     out = out or sys.stdout
     out.write("*Promotions*\n")
+
+    build_tag_table = [[build, tag] for tag in promoted_builds for build in promoted_builds[tag]]
+    build_tag_table.sort(key=lambda x: x[0].nvr)
+
     nvrs_no_dist = set()
-    table = "|| Tag || Build ||\n"
-    for tag in sorted(promoted_builds):
-        for build in promoted_builds[tag]:
-            uri = kojihelper.get_build_uri(build.nvr)
-            table += "| %s | [%s|%s] |\n" % (tag, build.nvr, uri)
-            nvrs_no_dist.add(build.nvr_no_dist)
+    table_str = "|| Build || Tag ||\n"
+    for build, tag in build_tag_table:
+        uri = kojihelper.get_build_uri(build.nvr)
+        table_str += "| [%s|%s] | %s |\n" % (build.nvr, uri, tag)
+        nvrs_no_dist.add(build.nvr_no_dist)
+
     out.write("Promoted %s to %s\n" % (", ".join(sorted(nvrs_no_dist)), ", ".join([x.to_tag_hint % "el*" for x in routes])))
-    out.write(table)
+    out.write(table_str)
 
 
 #

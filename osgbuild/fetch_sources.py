67ab4f77dfc8a0aca63bd38d2340d54b69e9a943
Matyas Selmeci <matyas@cs.wisc.edu>
2018-03-13 18:36:11 -0500

    Turn multiple spec files into an error

--- osgbuild/fetch_sources.py
+++ osgbuild/fetch_sources.py
@@ -315,7 +315,7 @@ def fetch(package_dir,
     if not spec_filenames:
         raise GlobNotFoundError(spec_glob)
     if len(spec_filenames) > 1:
-        log.warning("Multiple spec files found; using %r", spec_filenames[0])
+        raise Error("Multiple spec files found: " + ", ".join(spec_filenames))
 
     return spec_filenames[0]
 # end of fetch

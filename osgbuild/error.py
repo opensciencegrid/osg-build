e65c583f897abf4de6a50b46f1f5e7ecccd58b64
Matyas Selmeci <matyas@cs.wisc.edu>
2023-09-26 19:46:26 -0500

    Docstrings for errors

--- osgbuild/error.py
+++ osgbuild/error.py
@@ -19,28 +19,33 @@ class Error(Exception):
 
 
 class SVNError(Error):
+    """Error doing SVN actions"""
     def __init__(self, msg):
         Error.__init__(self, "SVN error: %s" % msg)
 
 
 class GitError(Error):
+    """Error doing Git actions"""
     def __init__(self, msg):
         Error.__init__(self, "git error: %s" % msg)
 
 
 class GlobNotFoundError(Error):
+    """Error raised when no file matching a given glob was found."""
     def __init__(self, globtext):
         msg = "Couldn't find file matching '%s'." % globtext
         Error.__init__(self, msg)
 
 
 class FileNotFoundInSearchPathError(Error):
+    """Error raised when a required file wasn't found in the search path."""
     def __init__(self, fname, searchpath):
         msg = "Couldn't find file named '%s' in search path %s." % (fname, os.pathsep.join(searchpath))
         Error.__init__(self, msg)
 
 
 class ProgramNotFoundError(Error):
+    """Error raised when a required program wasn't found."""
     def __init__(self, program):
         msg = "Couldn't find required program '%s'." % program
         if "/" not in program:
@@ -50,31 +55,37 @@ class ProgramNotFoundError(Error):
 
 
 class OSGBuildError(Error):
+    """Error in the build step"""
     def __init__(self, msg, tb=None):
         Error.__init__(self, "Error in build step: " + msg, tb)
 
 
 class OSGPrebuildError(Error):
+    """Error in the prebuild step"""
     def __init__(self, msg, tb=None):
         Error.__init__(self, "Error in pre-build step: " + msg, tb)
 
 
 class UsageError(Error):
+    """Error raised when invalid arguments are passed"""
     def __init__(self, msg, tb=None):
         Error.__init__(self, "Usage error: " + msg + "\n", tb)
 
 
 class KojiError(Error):
+    """Error in the koji task or otherwise dealing with Koji"""
     def __init__(self, msg, tb=None):
         Error.__init__(self, "Koji error: " + msg, tb)
 
 
 class MockError(Error):
+    """Error in the mock task or otherwise dealing with Mock."""
     def __init__(self, msg, tb=None):
         Error.__init__(self, "Mock error: " + msg, tb)
 
 
 class ClientCertError(Error):
+    """Error raised when there is an issue in the client cert."""
     def __init__(self, filename, msg, tb=None):
         Error.__init__(self, "Client cert error: %s (%s)" % (msg, filename), tb)
 
